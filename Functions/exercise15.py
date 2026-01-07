dc9ecfd6fed3a43a40fc3b81ddb9d05ad72a8ae2
Robert <110719811+RobertCwenar@users.noreply.github.com>
2026-01-07 20:59:21 +0000

    Add examples for 'any' and 'all'

--- Functions/exercise15.py
+++ Functions/exercise15.py
@@ -33,7 +33,7 @@ b.append(4)
 
 print(a)
 print(b)
-'''
+
 # exercise 15.3
 
 a = [[1, 2], [3, 4]]
@@ -41,3 +41,39 @@ b = a.copy()
 
 b[0].append(100)
 print(a)
+'''
+
+# exercise 15.4
+# Użj funkcji any() aby określicz czy lista ma liczby parzyste
+
+numbers1 = [1, 3, 5, 7, 9]
+numbers2 = [1, 2, 3, 4, 5]
+numbers3 = [2, 3, 6, 9]
+
+def any_even(lista):
+   return any ([nr % 2 == 0 for nr in lista])
+
+def all_even(lista):
+   return all ([nr % 2 == 0 for nr in lista])
+
+print(any_even(numbers1))
+print(any_even(numbers2))
+print(any_even(numbers3))
+
+if (any_even(numbers1)):
+    print("Lista numbers1 ma liczby parzyste")
+else:
+    print("Lista numbers1 nie ma liczb parzystych")
+
+if (any_even(numbers2)):
+    print("Lista numbers2 ma liczby parzyste")
+else:
+    print("Lista numbers2 nie ma liczb parzystych")
+
+if (any_even(numbers3)):
+    print("Lista numbers3 ma liczby parzyste")
+else:
+    print("Lista numbers3 nie ma liczb parzystych")
+
+# any - jakikolwiek e- funkcja any Sprawdza czy jakikolwiek element w iterowalnym jest prawdziwy
+
