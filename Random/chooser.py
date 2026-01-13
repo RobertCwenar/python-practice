5c2072ea7a9149a214ef491d3354e5ee5958173c
Robert <110719811+RobertCwenar@users.noreply.github.com>
2026-01-13 10:05:00 +0000

    Add choice and choices in movie_list and create simple choose cards for 2 players

---
+++ Random/chooser.py
@@ -0,0 +1,54 @@
+""""
+choice - zwraca losowy element
+choices = zwraca listę elementów i ma większe możliwości
+
+
+import random 
+
+movie_list = [ "Title1", "Title2", "Title3", "Title4", "Title5", "Title6"]
+event = ["death", "win", "lose", "gold loss", "lifeloss"]
+treasure_chest = ["green", "purple", "orange", "gold", "legendary"]
+
+from collections import Counter
+print(Counter(random.choices(treasure_chest, [0.70, 0.15, 0.10, 0.04, 0.01], k = 1000)))
+"""
+
+import random
+
+#random.shuffle() tasuje listę w miejscu – czyli zmienia kolejność elementów w liście losowo, nie tworząc nowej listy.
+
+# Stworz liste która przechowuje karty jednego gracza i drugiego gracza tak zeby sciagało z głownej listy 
+
+# variables 
+
+suits = ["♠", "♥", "♦", "♣"]
+card_lists = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+
+# Loop for 
+deck = [card + suit for suit in suits for card in card_lists]
+
+round_number = 1 
+while round_number <= len(deck) >= 10:
+    player1 =[deck.pop() for _ in range(5)] 
+    player2 = [deck.pop() for _ in range(5)]
+
+
+
+    random.shuffle(deck)
+    round_number = round_number + 1
+    print(f"Runda: {round_number}")
+    print(player1)
+    print(player2)
+
+print(f"Cards left: {len(deck)} -> {deck}")
+
+
+"""
+import random
+# sample - probka
+
+def choose_random_numbers(amount, total_amount):
+    print(random.sample(range(total_amount + 1), amount))
+
+choose_random_numbers(6,49)
+"""
